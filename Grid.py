4b90409d564a016717f672334ee2dc7fa529bae4
archi <archisa28@gmail.com>
2024-02-08 10:39:26 -0500

    added size to Grid.py

--- Grid.py
+++ Grid.py
@@ -1,10 +1,11 @@
 import random
-grid = [[0 for i in range(10)] for j in range(10)]
-grid = [[random.randint(0,1) for i in range(10)] for j in range(10)]
+size = 5
+grid = [[0 for i in range(size)] for j in range(size)]
+grid = [[random.randint(0,1) for i in range(size)] for j in range(size)]
 grid[0][0] = 0
-grid[9][9] = 0
+grid[size-1][size-1] = 0
 
-for i in range(10):
-    for j in range(10):
+for i in range(size):
+    for j in range(size):
         print(grid[i][j], end = ' ')
     print()
