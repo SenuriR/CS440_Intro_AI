fe2b30140f945d6964a4485a9a1b78e7b43f50c0
Senuri Rupasinghe <senuri.rupasinghe@rutgers.edu>
2024-02-15 10:55:31 -0500

    edits to node class

--- Node.py
+++ Node.py
@@ -1,12 +1,9 @@
 class Node:
     def __init__(self, x=None, y=None, next_node=None,
-    parent=None, g_value=None, h_value=None, f_value=None):
-        self.data = (x,y)
+    i=None, j=None, g_value=None, h_value=None, f_value=None):
+        self.data = [x, y]
         self.next_node = []
-        self.parent = (i,j)
+        self.parent = [i, j]
         self.g_value = g_value
         self.h_value = h_value
         self.f_value = f_value
-
-Node1 = Node(data=42)
-print(Node1.data)
