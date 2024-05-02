568dfd7a14d3f218d972f9960537c1bc6c5a9084
Senuri Rupasinghe <senuri.rupasinghe@rutgers.edu>
2024-05-02 08:55:55 -0400

    cleaning up nn

--- final_project_face_and_digit_classification/classification/neuralNetwork.py
+++ final_project_face_and_digit_classification/classification/neuralNetwork.py
@@ -3,37 +3,40 @@ import scipy.optimize as opt
 
 class NeuralNetworkClassifier():
     def __init__(self, legalLabels, numInput, numHidden, numOutput, numData, l):
+        
+        # basic intialization
         self.numInput = numInput
         self.numHidden = numHidden
         self.numOutput = numOutput
         self.numData = numData
         self.legalLabels = legalLabels
+        self.numFeatures = numInput
+        self.numClasses = numOutput
         self.l = l
 
-        inputActivationDims = (self.numInput + 1, numData)
+        # step 1 - randomly initialize weights
+        self.inputWeights = np.random.rand(self.numHidden, self.numInput + 1)
+        self.outputweights = np.random.rand(self.numOutput, self.numHidden + 1)
+
+        # initialize input, hidden, and output activation functions
+        inputActivationDims = (self.numInput + 1, numData) # +1 for bias
         hiddenActivationDims = (self.numHidden + 1, numData)
         outputActivationDims = (self.numOutput, numData)
-
         self.inputActivation = np.ones(inputActivationDims)
         self.hiddenActivation = np.ones(hiddenActivationDims)
         self.outputActivation = np.ones(outputActivationDims)
 
-        biasVectorDims = (1, numData)
-        self.biasVector = np.ones(biasVectorDims)
-
+        # initialize input and output delta matrices
         inputDeltaDims = (self.numHidden, self.numInput+1)
         outputDeltaDims = (self.numOutput, self.numHidden+1)
         self.inputDelta = np.zeros(inputDeltaDims)
         self.outputDelta = np.zeros(outputDeltaDims)
 
-        # randomly assign weights - not sure if I did this right, need to check this.
-        self.hiddenRandomFactor = np.sqrt(6.0 / (self.numInput + self.numHidden))
-        self.outputRandomFactor = np.sqrt(6.0 / (self.numInput + self.numOutput))
-        self.inputWeights = np.random.rand(self.numHidden, self.numInput + 1) * 2 * self.hiddenRandomFactor - self.hiddenRandomFactor
-        self.outputweights = np.random.rand(self.numOutput, self.numHidden + 1) * 2 * self.outputRandomFactor - self.outputRandomFactor
+        # also bias vector
+        biasVectorDims = (1, numData)
+        self.biasVector = np.ones(biasVectorDims)
 
-        self.l = l
-
+    # (step 2 - implement forward propagation to get h_theta_Xi for any instance Xi)
     def forwardProp(self):
         print("Implement forward propagation here")
 
@@ -48,10 +51,13 @@ class NeuralNetworkClassifier():
     '''
     def backProp(self, theta):
         # First step
-        inPreReshape = theta[0:self.numHidden * (self.numInput + 1)]
-        outPreReshape = theta[-self.numOutput * (self.numHidden + 1):]
-        self.inputWeights = inPreReshape.reshape((self.numHidden, self.numInput + 1))
-        self.outputWeights = outPreReshape.reshape((self.numOutput, self.numHidden + 1))
+        weights_in_hidden = theta[0:self.numHidden * (self.numInput + 1)]
+        weights_hidden_out = theta[-self.numOutput * (self.numHidden + 1):]
+        dim_hidden_in = (self.numHidden, self.numInput + 1)
+        dim_out_hidden = (self.numOutput, self.numHidden + 1)
+        # now we can successfully define matrices: inputWeights - weights btw input and hidden and outputWeights - weights btw hidden and output
+        self.inputWeights = weights_in_hidden.reshape(dim_hidden_in)
+        self.outputWeights = weights_hidden_out.reshape(dim_out_hidden)
 
         # Second step
         outputError = self.outputActivation - self.trueOut
@@ -101,7 +107,11 @@ class NeuralNetworkClassifier():
         self.outputWeights = outPreReshape.reshape((self.numOutput, self.numHidden + 1))
 
     def classification(self, testData):
-        print("Implement final classification using test data here")
+        print("Implement classification here.")
+
+    def costFunction(self, input, output):
+        return -1 * ((input * np.log(output)) + ((1 - input)*np.log(1 - output)))
+
     
 #################### HELPER FUNCTIONS ####################
 
